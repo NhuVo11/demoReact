e82a12ca7fbd7e7d47379ec8918d6f0f6cfd6935
18130169-VoNguyenKhaNhu <18130169@st.hmuaf.edu.vn>
2022-06-01 16:02:10 +0700

    demo react page home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Products from "./components/Products";
+import Services from "./components/Services";
+import SignUp from "./components/SignUp";
 import { Home } from "./pages/Home";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import "../App.css";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
 
 export function Home() {
   return (
     <>
       <HeroSection />
+      <Card />
+      <Footer />
     </>
   );
 }
